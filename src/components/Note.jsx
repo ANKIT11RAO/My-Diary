da7806e097f7a72d1631404e57a83a8d3cba9978
ANKIT11RAO <81503627+ANKIT11RAO@users.noreply.github.com>
2021-10-07 21:13:20 +0530

    Add files via upload

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Date</h1>
+      <p>Diary content</p>
+    </div>
+  );
+}
+
+export default Note;
